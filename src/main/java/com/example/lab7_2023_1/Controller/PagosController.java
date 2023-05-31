7afeb5f016d438de9c716b3f0fe1b169254ec129
Ana Sofia <alejandro.as@pucp.edu.pe>
2023-05-31 15:06:03 -0500

    3.b Registrar Pago

--- src/main/java/com/example/lab7_2023_1/Controller/PagosController.java
+++ src/main/java/com/example/lab7_2023_1/Controller/PagosController.java
@@ -1,12 +1,16 @@
 package com.example.lab7_2023_1.Controller;
 
 import com.example.lab7_2023_1.Entity.Pagos;
+import com.example.lab7_2023_1.Entity.Solicitudes;
 import com.example.lab7_2023_1.Entity.Usuarios;
 import com.example.lab7_2023_1.Repository.PagosRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
 
 @RestController //me añade el @ResponseBody
@@ -19,5 +23,33 @@ public class PagosController {
     public List<Pagos> listarPagos(){
         List<Pagos> list = pagosRepository.findAll();
         return list;
+    }
+
+    @PostMapping("/registrarPago")
+    public ResponseEntity<HashMap<String, Object>> registrarPago(
+            @RequestBody Pagos pagos,
+            @RequestParam(value = "fetchId", required = false) boolean fetchId) {
+
+        HashMap<String, Object> responseJson = new HashMap<>();
+
+        pagosRepository.save(pagos);
+        if(fetchId){
+            responseJson.put("id",pagos.getId());
+        }
+        int id = pagos.getId();
+        responseJson.put("id", id);
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseJson);
+    }
+
+
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<HashMap<String,String>> gestionException(HttpServletRequest request){
+        HashMap<String,String> responseMap = new HashMap<>();
+        if(request.getMethod().equals("POST")|| request.getMethod().equals("PUT")){
+            responseMap.put("estado","error");
+            responseMap.put("msg","Debe enviar un pago");
+        }
+        return ResponseEntity.badRequest().body(responseMap);
     }
 }
