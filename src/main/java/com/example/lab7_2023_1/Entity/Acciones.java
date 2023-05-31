4a8223ddcc5c975eb730ba072afa7a38d2ff7f9e
Ana Sofia <alejandro.as@pucp.edu.pe>
2023-05-31 15:14:14 -0500

    4. Guardar acciones

---
+++ src/main/java/com/example/lab7_2023_1/Entity/Acciones.java
@@ -0,0 +1,30 @@
+package com.example.lab7_2023_1.Entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "acciones")
+@Getter
+@Setter
+public class Acciones {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "monto")
+    private Double monto;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime fecha;
+
+    @ManyToOne
+    @JoinColumn(name = "usuarios_id", nullable = false)
+    private Usuarios usuarios_id;
+
+}
